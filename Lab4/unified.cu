53905f92f78c1c20e37d66a69e9b4bd2ac886f9d
mikibak <107919074+mikibak@users.noreply.github.com>
2025-12-21 16:36:31 +0100

    Implement prime check with unified memory

--- Lab4/unified.cu
+++ Lab4/unified.cu
@@ -1,57 +1,56 @@
 /*
 CUDA - generation and sum of arithmetic progression build of 10240000 elements a1=0 r=1 with Unified Memory
 */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <cuda_runtime.h>
 
-__host__
-void errorexit(const char *s) {
-    printf("\n%s",s);	
-    exit(EXIT_FAILURE);	 	
+typedef int idx_t;
+
+#define cudaCheckError(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+  if (code != cudaSuccess)
+  {
+    fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+    if (abort) exit(code);
+  }
 }
 
-//elements generation
-__global__ 
-void calculate(int *result) {
-    int my_index=blockIdx.x*blockDim.x+threadIdx.x;
-    result[my_index]=my_index;
+
+__global__
+void check_prime(int n, int *is_prime) {
+  idx_t idx = blockIdx.x * blockDim.x + threadIdx.x + 2; // start from 2
+  if (idx > n/2) return;
+  if (n % idx == 0) {
+    *is_prime = 0;
+  }
 }
 
 
-int main(int argc,char **argv) {
+int main(int argc, char **argv) {
+    int n = 0;
+    printf("Enter a number for prime check:\n");
+    scanf("%d", &n);
+    
+    int threadsinblock = 1024;
+    int max_divisor = n / 2;
+    int blocksingrid = (max_divisor + threadsinblock - 1) / threadsinblock;
 
-    long long result;
-    int threadsinblock=1024;
-    int blocksingrid=10000;	
+    int *is_prime;
+    cudaCheckError(cudaMallocManaged(&is_prime, sizeof(int)));
+    *is_prime = 1;
 
-    int size = threadsinblock*blocksingrid;
+    check_prime<<<blocksingrid, threadsinblock>>>(n, is_prime);
+    cudaCheckError(cudaGetLastError());
 
-    int *results;
-
-    //unified memory allocation - available for host and device
-    if (cudaSuccess!=cudaMallocManaged(&results,size*sizeof(int)))
-      errorexit("Error allocating memory on the GPU");
-
-    //call to GPU - kernel execution 
-    calculate<<<blocksingrid,threadsinblock>>>(results);
-
-    if (cudaSuccess!=cudaGetLastError())
-      errorexit("Error during kernel launch");
-  
-    //device synchronization to ensure that data in memory is ready
     cudaDeviceSynchronize();
 
-    //calculate sum of all elements
-    result=0;
-    for(int i=0;i<size;i++) {
-      result = result + results[i];
-    }
+    if (*is_prime)
+        printf("\n%d is a prime number.\n", n);
+    else
+        printf("\n%d is NOT a prime number.\n", n);
 
-    printf("\nSum of all elements is  %lld\n",result);
-
-    //free memory
-    if (cudaSuccess!=cudaFree(results))
-      errorexit("Error when deallocating space on the GPU");
-
+    cudaCheckError(cudaFree(is_prime));
 }
